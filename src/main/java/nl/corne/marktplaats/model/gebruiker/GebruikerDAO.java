fe769a563828fce69e64e6070f993e5617167ab4
cornevandergeer <cornevandergeer@gmail.com>
2023-05-09 16:07:15 +0200

    removed gebruiker oud, rol oud, gebruikerdao oud

--- src/main/java/nl/corne/marktplaats/model/gebruiker/GebruikerDAO.java
+++ src/main/java/nl/corne/marktplaats/model/gebruiker/GebruikerDAO.java
@@ -73,6 +73,17 @@ public class GebruikerDAO implements GebruikerDAOInterface {
         }
     }
 
+    public Gebruiker selectGebruikerByNaam(String voornaam) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        String query = "select g from Gebruiker g where voornaam = :n";
+        Gebruiker gebruiker = em.createQuery(query, Gebruiker.class).
+                setParameter("n", voornaam).
+                getSingleResult();
+        transaction.commit();
+        return gebruiker;
+    }
+
     public void close(){
         em.close();
     }
--- src/main/java/nl/corne/marktplaats/model/gebruiker/RolOud.java
+++
@@ -1,5 +0,0 @@
-package nl.corne.marktplaats.model.gebruiker;
-
-public enum RolOud {
-    HANDELAAR, MAGAZIJNMEDEWERKER, BEHEERDER
-}
