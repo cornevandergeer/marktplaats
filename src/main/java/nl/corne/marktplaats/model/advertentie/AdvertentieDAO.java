f120a098287f7b168c73fbc5723e0f571b9e0bc3
Leon L.M.F. Crombach <lmf.crombach@belastingdienst.nl>
2023-05-11 09:49:21 +0200

    Added body to method get/insert/update in class AdvertentieDAO

--- src/main/java/nl/corne/marktplaats/model/advertentie/AdvertentieDAO.java
+++ src/main/java/nl/corne/marktplaats/model/advertentie/AdvertentieDAO.java
@@ -1,17 +1,31 @@
 package nl.corne.marktplaats.model.advertentie;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
+@Singleton
 public class AdvertentieDAO implements AdvertentieDAOInterface {
+
+    @Inject
+    private EntityManager em;
+    EntityTransaction transaction = em.getTransaction();
 
     @Override
     public Advertentie get(int id) {
-        return null;
+        return em.find(Advertentie.class, id);
     }
 
     @Override
     public List<Advertentie> getAll() {
-        return null;
+       return em.createNamedQuery("Advertentie.findAll", Advertentie.class).getResultList();
     }
 
     @Override
@@ -21,12 +35,28 @@ public class AdvertentieDAO implements AdvertentieDAOInterface {
 
     @Override
     public void insert(Advertentie advertentie) {
-
+        transaction.begin();
+        try {
+            em.persist(advertentie);
+            transaction.commit();
+        } catch(Exception e){
+            log.error("Er ging iets mis: ", e);
+            transaction.rollback();
+        }
     }
 
     @Override
     public int update(Advertentie advertentie) {
-        return 0;
+        transaction.begin();
+        try {
+            em.merge(advertentie);
+            transaction.commit();
+            return 1;
+        } catch (Exception e){
+            log.error("Er ging iets mis: ", e);
+            transaction.rollback();
+            return 0;
+        }
     }
 
     @Override
